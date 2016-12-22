f913b11b83eea61d08bdc5f30706ff6b584e0838
HoinJang <ghdlsfl@naver.com>
2016-12-22 17:21:10 +0900

    12/22

--- Project/MyGame_JHI.py
+++ Project/MyGame_JHI.py
@@ -1,5 +1,11 @@
-import Framework_JHI
+import os
+import platform
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
 
 import Start_State_JHI
+import Framework_JHI
 
 Framework_JHI.run(Start_State_JHI)
